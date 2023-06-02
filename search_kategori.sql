7304b83e0d4b25fd5649f09f34880531a112e403
William Nehemia <90948834+WilliamNehemia6182001041@users.noreply.github.com>
2023-06-02 16:04:09 +0700

    upload search kategori

---
+++ search_kategori.sql
@@ -0,0 +1,81 @@
+CREATE PROCEDURE searchKategori
+    @input VARCHAR(255)
+AS
+    CREATE TABLE #tag
+    (
+        tag VARCHAR(255)
+    )
+
+    CREATE TABLE #kategoriSesuaiTag
+    (
+        idKategori int
+    )
+
+    CREATE TABLE #idArtikelSesuaiTag
+    (
+        idArtikel int
+    )
+
+    CREATE TABLE #artikelSesuaiTag
+    (
+        namaArtikel VARCHAR(255),
+        isi TEXT,
+        namaPenulis VARCHAR(255),
+        statusArtikel VARCHAR(15),
+        tipe VARCHAR(7),
+        tanggal DATETIME
+    )
+
+    INSERT INTO #tag
+    SELECT
+        value
+    FROM
+        STRING_SPLIT(@input, ',')
+    
+    INSERT INTO #kategoriSesuaiTag
+    SELECT
+        idKategori
+    FROM 
+        Kategori
+    INNER JOIN
+        #tag
+    ON 
+        LOWER(Kategori.Topic) = LOWER(#tag.tag)
+
+    INSERT INTO #idArtikelSesuaiTag
+    SELECT
+        DISTINCT(DaftarKategoriArtikel.idArtikel)
+    FROM
+        DaftarKategoriArtikel
+    CROSS JOIN
+        #kategoriSesuaiTag
+    WHERE 
+        #kategoriSesuaiTag.idKategori = DaftarKategoriArtikel.idKategori
+
+    INSERT INTO #artikelSesuaiTag
+    SELECT
+        namaArtikel,
+        isi,
+        namaPenulis,
+        [status],
+        tipe,
+        tanggal
+    FROM
+        Artikel
+    INNER JOIN 
+        #idArtikelSesuaiTag
+    ON
+        Artikel.idArtikel = #idArtikelSesuaiTag.idArtikel
+    
+    SELECT 
+        namaArtikel ,
+        isi ,
+        namaPenulis ,
+        statusArtikel ,
+        tipe ,
+        tanggal 
+    FROM
+        #artikelSesuaiTag
+
+EXEC searchKategori 'deep learning,machinelearning'
+    
