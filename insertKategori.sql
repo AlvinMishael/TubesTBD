239c09cd5f7ef656893e1878bf0504a259daa2bd
5thPhantom <6182001047@student.unpar.ac.id>
2023-06-07 08:17:09 +0700

    Update typo

--- insertKategori.sql
+++ insertKategori.sql
@@ -1,7 +1,7 @@
 ALTER PROCEDURE insertKategori
     @namaKategori VARCHAR(255)
 AS
-    INSERT INTO Kategori (namaKategori)
+    INSERT INTO Kategori (topic)
     VALUES (@namaKategori)
 
 EXEC insertKategori 'KNN'
